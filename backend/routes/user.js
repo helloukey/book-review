8c0d071577f239f93618f8012ff52a3ec3f35e8d
Kunal <kunalukey32@gmail.com>
2024-07-09 09:47:34 +0530

    removed unused import from user.js file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,5 @@
 const userRoute = require("express").Router();
 const { registerUser } = require("../controllers/userController");
-const sendMail = require("../utils/mail");
 
 userRoute.post("/register", registerUser);
 
