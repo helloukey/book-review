f35a6c59d3a108887b594b9d2906847ea1dad9c1
Kunal <kunalukey32@gmail.com>
2024-07-10 22:06:15 +0530

    added verified check for requireAuth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -16,6 +16,14 @@ const requireAuth = async (req, res, next) => {
     if (!user) {
       res.redirect("/login");
     }
+    // check if the user has verified their account
+    if (!user.verified) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please verify your account" });
+    }
+
+    // set the user in the request object
     req.user = user;
     next();
   } catch (error) {
